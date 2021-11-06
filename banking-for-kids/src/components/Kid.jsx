66353924094a919107165efd38c35a355667252b
SavageGarrett <carder.320@osu.edu>
2021-11-06 19:15:50 -0400

    Kid Template

--- banking-for-kids/src/components/Kid.jsx
+++ banking-for-kids/src/components/Kid.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Container, Row, Col, Text } from "react-bootstrap";
+import { Container, Row, Col, Text, Image } from "react-bootstrap";
 import { Grid, Typography, Link as ButtonLink } from "@material-ui/core";
 
 const Kid = () => {
   return (
     <Container>
       <Row>
-        <Typography>I am a kid!</Typography>
+        <Col>
+          {/* Profile Image and Name */}
+          <Image style={{width: "120px", height: "120px", borderRadius: "60px"}} src="img/kid-1.jpg"></Image>
+          <Typography>Harold Smith</Typography>
+          
+          <Row>
+            {/* Savings Balance */}
+            <Col>
+              <Typography>Checking:</Typography>
+              <Typography>$ 2.32</Typography>
+            </Col>
+            
+            {/* Checking Balance */}
+            <Col>
+              <Typography>Savings:</Typography>
+              <Typography>$ 3.24</Typography>
+            </Col>
+          </Row>
+          
+        </Col>
+        <Col>
+
+        </Col>
       </Row>
     </Container>
   );

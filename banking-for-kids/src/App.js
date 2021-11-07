a5538d2300d3b6ec89bed3ad46351628c448be74
allison-broccolo <83963548+allison-broccolo@users.noreply.github.com>
2021-11-07 03:04:26 -0500

    get kid data form backend

--- banking-for-kids/src/App.js
+++ banking-for-kids/src/App.js
@@ -6,28 +6,48 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import { Toolbar, AppBar, Button, Grid } from "@material-ui/core";
 import theme from "./theme";
 
-var axios = require('axios')
+var axios = require("axios");
 
 function App() {
   /* True if Kid, False if Parent */
   const [kid, setKid] = useState(true);
-  const [tasks, setTasks] = useState(Array(Array(5)))
-  
+  const [tasks, setTasks] = useState(Array(Array(5)));
 
+  useEffect(() => {
+    var data = JSON.stringify({
+      user_id: 2,
+    });
+    var config = {
+      method: "GET",
+      url: "https://c7j9xga7y9.execute-api.us-east-1.amazonaws.com/default/gettersetter",
+      headers: {
+        "x-api-key": "hkW0Vha0KA8GH60qfJmv88h8EAJx8VNp5Fgpem2G",
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
   const getTasksFromDB = () => {
     return new Promise((resolve, reject) => {
       var data = JSON.stringify({});
-  
+
       var config = {
-        method: 'PATCH',
-        url: 'https://1u6xfou096.execute-api.us-east-1.amazonaws.com/default/gettersetter_tasks',
-        headers: { 
-          'x-api-key': 'r1rC7mLDKb1Bxc5neUsNt7JtxbSYF9ti3yYUMjkE',
-          'Content-Type': 'application/json'
+        method: "PATCH",
+        url: "https://1u6xfou096.execute-api.us-east-1.amazonaws.com/default/gettersetter_tasks",
+        headers: {
+          "x-api-key": "r1rC7mLDKb1Bxc5neUsNt7JtxbSYF9ti3yYUMjkE",
+          "Content-Type": "application/json",
         },
-        data : data
+        data: data,
       };
-  
+
       axios(config)
         .then(function (response) {
           resolve(response.data);
@@ -35,19 +55,19 @@ function App() {
         .catch(function (error) {
           reject(error);
         });
-    })
-  }
+    });
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getTasksFromDB()
-      .then(response => {
-        console.log(response.data)
+      .then((response) => {
+        console.log(response.data);
       })
-      .catch(error => {
-        console.log(error)
-      })
-    console.log(tasks)
-  }, [])
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(tasks);
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -66,7 +86,7 @@ function App() {
             </Grid>
           </Toolbar>
         </AppBar>
-        {kid ? <Kid/> : <Parent />}
+        {kid ? <Kid /> : <Parent />}
       </div>
     </ThemeProvider>
   );
